7709bba27e3dbd5d3e2bd68cd26ab0aca44efd68
วีรวิชญ์ สิงห์วาหะนนท์ <weers571@epicc.postbox.in.th>
2022-07-04 16:46:57 +0700

    Add Log4j Azure - Example code
    Add Log4j Azure  - Example code

--- src/main/java/com/api/sample/APISample.java
+++ src/main/java/com/api/sample/APISample.java
@@ -2,12 +2,19 @@ package com.api.sample;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import org.apache.log4j.Logger;
 
 @WebService
 public class APISample {
 
+	private static final Logger LOGGER = Logger.getLogger(APISample.class);
+	
 	@WebMethod
 	public String HelloMyName(String name) {
+		
+		LOGGER.info("Initializing ExampleLog4j application : "+name);
+		LOGGER.error("Error ExampleLog4j application : "+name);
+		LOGGER.warn("Warning ExampleLog4j application : "+name);
 		
 		return "Hello "+name;
 	}
